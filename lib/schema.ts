829d331a64e42aad3005757f502b96dce9274d5f
Michelle Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-02-07 00:10:08 -0500

    Update schema.ts

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,376 +1,89 @@
-"use client";
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import { toast } from "@/components/ui/use-toast";
-import { createBrowserSupabaseClient } from "@/lib/client-utils";
-import type { Database } from "@/lib/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useEffect, useState, type BaseSyntheticEvent, type MouseEvent } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-type Species = Database["public"]["Tables"]["species"]["Row"];
-
-// We use zod (z) to define a schema for the "Add species" form.
-// zod handles validation of the input values with methods like .string(), .nullable(). It also processes the form inputs with .transform() before the inputs are sent to the database.
-
-// Define kingdom enum for use in Zod schema and displaying dropdown options in the form
-const kingdoms = z.enum(["Animalia", "Plantae", "Fungi", "Protista", "Archaea", "Bacteria"]);
-
-// Use Zod to define the shape + requirements of a Species entry; used in form validation
-const speciesSchema = z.object({
-  author: z.string(),
-  scientific_name: z
-    .string()
-    .trim()
-    .min(1)
-    .transform((val) => val?.trim()),
-  common_name: z
-    .string()
-    .nullable()
-    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
-    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
-  kingdom: kingdoms,
-  total_population: z.number().int().positive().min(1).nullable(),
-  image: z
-    .string()
-    .url()
-    .nullable()
-    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
-    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
-  description: z
-    .string()
-    .nullable()
-    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
-    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
-  endangered: z.boolean(),
-});
-
-type FormData = z.infer<typeof speciesSchema>;
-
-// Default values for the form fields.
-/* Because the react-hook-form (RHF) used here is a controlled form (not an uncontrolled form),
-fields that are nullable/not required should explicitly be set to `null` by default.
-Otherwise, they will be `undefined` by default, which will raise warnings because `undefined` conflicts with controlled components.
-All form fields should be set to non-undefined default values.
-Read more here: https://legacy.react-hook-form.com/api/useform/
-*/
-
-export default function SpeciesDetailsDialog({ species, currentUser }: { species: Species; currentUser: string }) {
-  const router = useRouter();
-
-  // Control open/closed state of the dialog
-  const [isEditing, setIsEditing] = useState(false);
-  const [authorInfo, setAuthorInfo] = useState<null | {
-    email: string;
-    display_name: string;
-    biography: string | null;
-  }>(null);
-
-  const defaultValues: Partial<FormData> = {
-    scientific_name: species.scientific_name,
-    common_name: species.common_name,
-    kingdom: species.kingdom,
-    total_population: species.total_population,
-    image: species.image,
-    description: species.description,
-    endangered: species.endangered,
-  };
-
-  useEffect(() => {
-    const fetchAuthorInfo = async () => {
-      const supabase = createBrowserSupabaseClient();
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("email, display_name, biography")
-        .eq("id", species.author)
-        .single();
-
-      if (error) {
-        console.error("Error fetching author info:", error.message);
-        return;
-      }
-
-      setAuthorInfo({ email: data.email, display_name: data.display_name, biography: data.biography });
+export interface Database {
+  public: {
+    Tables: {
+      profiles: {
+        Row: {
+          biography: string | null;
+          display_name: string;
+          email: string;
+          id: string;
+        };
+        Insert: {
+          biography?: string | null;
+          display_name: string;
+          email: string;
+          id: string;
+        };
+        Update: {
+          biography?: string | null;
+          display_name?: string;
+          email?: string;
+          id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "profiles_id_fkey";
+            columns: ["id"];
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      species: {
+        Row: {
+          author: string;
+          common_name: string | null;
+          description: string | null;
+          id: number;
+          image: string | null;
+          kingdom: Database["public"]["Enums"]["kingdom"];
+          scientific_name: string;
+          total_population: number | null;
+        };
+        Insert: {
+          author: string;
+          common_name?: string | null;
+          description?: string | null;
+          id?: number;
+          image?: string | null;
+          kingdom: Database["public"]["Enums"]["kingdom"];
+          scientific_name: string;
+          total_population?: number | null;
+          endangered?: boolean | null;
+        };
+        Update: {
+          author?: string;
+          common_name?: string | null;
+          description?: string | null;
+          id?: number;
+          image?: string | null;
+          kingdom?: Database["public"]["Enums"]["kingdom"];
+          scientific_name?: string;
+          total_population?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "species_author_fkey";
+            columns: ["author"];
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
     };
-
-    void fetchAuthorInfo();
-  }, [species.author]);
-
-  // Instantiate form functionality with React Hook Form, passing in the Zod schema (for validation) and default values
-  const form = useForm<FormData>({
-    resolver: zodResolver(speciesSchema),
-    defaultValues,
-    mode: "onChange",
-  });
-
-  const onSubmit = async (input: FormData) => {
-    const supabase = createBrowserSupabaseClient();
-    const { error } = await supabase.from("species").update(input).eq("id", species.id);
-
-    if (error) {
-      return toast({
-        title: "st went wrong",
-        description: error.message,
-        variant: "destructive",
-      });
-    }
-
-    setIsEditing(false);
-
-    form.reset(input);
-    router.refresh();
-
-    return toast({
-      title: "Changes saved!",
-      description: "Saved your changes to " + input.scientific_name + ".",
-    });
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      kingdom: "Animalia" | "Plantae" | "Fungi" | "Protista" | "Archaea" | "Bacteria";
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
   };
-
-  const startEditing = (e: MouseEvent) => {
-    e.preventDefault();
-    setIsEditing(true);
-  };
-
-  const handleCancel = (e: MouseEvent) => {
-    e.preventDefault();
-    // OK: true
-    // Cancel: false, return early
-    if (!window.confirm("Discard all changes?")) {
-      return;
-    }
-    form.reset(defaultValues);
-    setIsEditing(false);
-  };
-
-  const onDelete = async () => {
-    if (window.confirm("Are you sure you want to delete this species?")) {
-      const supabase = createBrowserSupabaseClient();
-      const { error } = await supabase.from("species").delete().eq("id", species.id);
-
-      if (error) {
-        toast({
-          title: "st went wrong",
-          description: error.message,
-          variant: "destructive",
-        });
-      }
-
-      router.refresh();
-
-      toast({
-        title: "Deletion saved!",
-        description: "Deleted " + species.scientific_name + " successfully.",
-      });
-    }
-  };
-
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="mt-3 w-full">Learn more</Button>
-      </DialogTrigger>
-      <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle>{species.scientific_name}</DialogTitle>
-        </DialogHeader>
-        <>
-          {authorInfo ? (
-            <div>
-              <p>Author info:</p>
-              <p>Email: {authorInfo.email}</p>
-              <p>Display Name: {authorInfo.display_name}</p>
-              <p>Biography: {authorInfo.biography}</p>
-            </div>
-          ) : null}
-        </>
-        <Form {...form}>
-          <form onSubmit={(e: BaseSyntheticEvent) => void form.handleSubmit(onSubmit)(e)}>
-            <div className="grid w-full items-center gap-4">
-              <FormField
-                control={form.control}
-                name="scientific_name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Scientific Name</FormLabel>
-                    <FormControl>
-                      <Input readOnly={!isEditing} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="common_name"
-                render={({ field }) => {
-                  const { value, ...rest } = field;
-                  return (
-                    <FormItem>
-                      <FormLabel>Common Name</FormLabel>
-                      <FormControl>
-                        <Input readOnly={!isEditing} value={value ?? ""} {...rest} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="kingdom"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Kingdom</FormLabel>
-                    <Select
-                      disabled={!isEditing}
-                      onValueChange={(value) => field.onChange(kingdoms.parse(value))}
-                      value={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select a kingdom" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectGroup>
-                          {kingdoms.options.map((kingdom, index) => (
-                            <SelectItem key={index} value={kingdom}>
-                              {kingdom}
-                            </SelectItem>
-                          ))}
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="total_population"
-                render={({ field }) => {
-                  const { value, ...rest } = field;
-                  return (
-                    <FormItem>
-                      <FormLabel>Total population</FormLabel>
-                      <FormControl>
-                        {/* Using shadcn/ui form with number: https://github.com/shadcn-ui/ui/issues/421 */}
-                        <Input
-                          type="number"
-                          readOnly={!isEditing}
-                          value={value ?? ""}
-                          placeholder="300000"
-                          {...rest}
-                          onChange={(event) => field.onChange(+event.target.value)}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="image"
-                render={({ field }) => {
-                  // We must extract value from field and convert a potential defaultValue of `null` to "" because inputs can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
-                  const { value, ...rest } = field;
-                  return (
-                    <FormItem>
-                      <FormLabel>Image URL</FormLabel>
-                      <FormControl>
-                        <Input
-                          readOnly={!isEditing}
-                          value={value ?? ""}
-                          placeholder="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/George_the_amazing_guinea_pig.jpg/440px-George_the_amazing_guinea_pig.jpg"
-                          {...rest}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => {
-                  // We must extract value from field and convert a potential defaultValue of `null` to "" because textareas can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
-                  const { value, ...rest } = field;
-                  return (
-                    <FormItem>
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          value={value ?? ""}
-                          readOnly={!isEditing}
-                          placeholder="The guinea pig or domestic guinea pig, also known as the cavy or domestic cavy, is a species of rodent belonging to the genus Cavia in the family Caviidae."
-                          {...rest}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="endangered"
-                render={({ field }) => (
-                  <FormItem className="flex items-center">
-                    <FormLabel className="mr-1">Endangered?</FormLabel>
-                    <FormControl>
-                      <div className="checkbox-wrapper">
-                        <Input
-                          disabled={!isEditing}
-                          type="checkbox"
-                          checked={field.value}
-                          onChange={(e) => field.onChange(e.target.checked)}
-                          className="form-checkbox h-6 w-6 rounded border border-black"
-                        />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {species.author === currentUser && (
-                <div className="flex">
-                  {isEditing ? (
-                    <>
-                      <Button type="submit" className="ml-1 mr-1 flex-auto">
-                        {" "}
-                        Confirm{" "}
-                      </Button>
-                      <Button onClick={handleCancel} type="submit" className="ml-1 mr-1 flex-auto" variant="secondary">
-                        {" "}
-                        Cancel
-                      </Button>
-                    </>
-                  ) : (
-                    <>
-                      <Button onClick={startEditing} type="button" className="ml-1 mr-1 flex-auto">
-                        {" "}
-                        Edit species{" "}
-                      </Button>
-                      <Button onClick={onDelete} type="button" className="ml-1 mr-1 flex-auto" variant="destructive">
-                        {" "}
-                        Delete species{" "}
-                      </Button>
-                    </>
-                  )}
-                </div>
-              )}
-            </div>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
-  );
 }
